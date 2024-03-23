33581b0fc6a1aeb217ffb855584dc968208a04ad
Casey Robb <carobb@redhat.com>
2024-03-23 11:20:25 -0600

    Add response to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-bw config server ${BW_HOST}
+bw config server ${BW_HOST} --response
 
 export BW_SESSION=$(bw login ${BW_USER} --passwordenv BW_PASSWORD --raw)
 
-bw unlock --check
+bw unlock --check --response --nointeraction
 
 echo 'Running `bw server` on port 8087'
-bw serve --hostname 0.0.0.0 #--disable-origin-protection
+bw serve --hostname 0.0.0.0 --response --nointeraction #--disable-origin-protection
